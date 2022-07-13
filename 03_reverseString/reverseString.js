516a0b60a6ad27c6ca39f0611ead9a17bf456e68
xb0n3 <102184534+xb0n3@users.noreply.github.com>
2022-07-13 16:47:27 +0200

    Solved exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let gnirts = '';
+  for (i = (string.length - 1); i >= 0; i--) {
+    gnirts += string[i];
+  }
+  return gnirts;
 };
 
 // Do not edit below this line
