e213905990811b2ae97450f6bc0c20ca60064466
xb0n3 <102184534+xb0n3@users.noreply.github.com>
2022-07-13 21:39:50 +0200

    Solved exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  const givenArray = args[0];
+  const newArray = [];
 
+  givenArray.forEach(item => {
+    if (!args.includes(item)) {
+      newArray.push(item);
+    };
+  });
+  return newArray;
 };
 
 // Do not edit below this line
