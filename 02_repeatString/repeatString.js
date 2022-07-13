9271bd8d6ca4a507f98780c2b531f60661b5b9c2
xb0n3 <102184534+xb0n3@users.noreply.github.com>
2022-07-13 16:29:43 +0200

    Solved exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let repeatedString = ''; 
+  if (num < 0) return 'ERROR';
+  while (num > 0) {
+    repeatedString += string;
+    num--;
+  }
+  return repeatedString;
 };
 
 // Do not edit below this line
