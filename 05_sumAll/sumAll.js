fd871fc3b975b2f1d9ca4275e2ac5a02808661c2
xb0n3 <102184534+xb0n3@users.noreply.github.com>
2022-07-15 13:25:36 +0200

    Solved exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  y = null;
+  z = null;
+  const array = [];
 
+  if (typeof a == 'number' && typeof b == 'number' && a > 0 && b > 0) {
+    if (a > b) z = a, y = b;
+    else y = a, z = b;
+
+    i = y;
+
+    while (i <= z) {
+      array.push(i)
+      i++;
+    };
+
+    return array.reduce((total, item) => {
+      return total + item;
+    }, 0);
+  } else {
+    return 'ERROR';
+  };
 };
 
 // Do not edit below this line
