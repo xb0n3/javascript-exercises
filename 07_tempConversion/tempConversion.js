0d0e95252d897c0cabe5c348fee1e15d32fb5b2a
xb0n3 <102184534+xb0n3@users.noreply.github.com>
2022-07-15 14:03:29 +0200

    Solved exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  celsius = (fahrenheit - 32) * (5 / 9);
+  return +celsius.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  fahrenheit = (celsius * (9 / 5)) + 32;
+  return +fahrenheit.toFixed(1);
 };
 
 // Do not edit below this line
